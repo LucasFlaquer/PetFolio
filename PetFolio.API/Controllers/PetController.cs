ac8b5ce2d48a85cb0bd9abad7a9d1dc46a707573
Lucas Flaquer <lucas.flaquer@gmail.com>
2025-05-08 13:44:47 -0300

    feat: created base structure and first controller

---
+++ PetFolio.API/Controllers/PetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFolio.Communication.Requests;
+using PetFolio.Communication.Responses;
+
+namespace PetFolio.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PetController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    {
+        // business logic
+
+        return Created();
+    }
+}
